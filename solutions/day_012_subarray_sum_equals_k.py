19fc4463cc84662025f7260934568645f98afcba
Areeb Syed <284557362+4reeb-5yed@users.noreply.github.com>
2026-07-13 17:48:53 +0530

    Day 012: Subarray Sum Equals K (two-pointers, medium) (#12)

---
+++ solutions/day_012_subarray_sum_equals_k.py
@@ -0,0 +1,10 @@
+def subarray_sum(nums, k):
+    count = 0
+    prefix_sum = 0
+    prefix_counts = {0: 1}
+    for num in nums:
+        prefix_sum += num
+        if prefix_sum - k in prefix_counts:
+            count += prefix_counts[prefix_sum - k]
+        prefix_counts[prefix_sum] = prefix_counts.get(prefix_sum, 0) + 1
+    return count
