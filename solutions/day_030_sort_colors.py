c2ca1c63853a286da710040288590914c8da78fa
4reeb-5yed <284557362+4reeb-5yed@users.noreply.github.com>
2026-07-31 11:35:43 +0000

    Day 030: Sort Colors (two-pointers, medium)

---
+++ solutions/day_030_sort_colors.py
@@ -0,0 +1,12 @@
+def sort_colors(nums):
+    left, mid, right = 0, 0, len(nums) - 1
+    while mid <= right:
+        if nums[mid] == 0:
+            nums[left], nums[mid] = nums[mid], nums[left]
+            left += 1
+            mid += 1
+        elif nums[mid] == 2:
+            nums[mid], nums[right] = nums[right], nums[mid]
+            right -= 1
+        else:
+            mid += 1
